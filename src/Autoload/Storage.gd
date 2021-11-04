3703ab875aa47c6a35c5f1896fba45d96ef3e2b3
Komdog <komdogart@gmail.com>
2021-11-04 16:15:04 -0400

    Categorized variables

--- src/Autoload/Storage.gd
+++ src/Autoload/Storage.gd
@@ -1,19 +1,22 @@
 extends Node
 
 var mod_path = OS.get_user_data_dir() + "/mods"
+var dialog_path = 'res://'
 
-var selected_beat: int = 0
-var selected_node: Node
+# var selected_beat: int = 0
+# var selected_node: Node
 
-var current_beat = 0.0
+var current_beat: int = 0
 var bpm: float = 128
-var loop_speed: float = 1
-var frame_rate_anim = 'loop_6'
+
+# var loop_speed: float = 1
+# var frame_rate_anim = 'loop_6'
 
 
+# Chart storage
 var written_notes = []
 
-var dialog_path = 'res://'
+
 
 # Configuration Variables
 var stage_name: String
@@ -27,7 +30,7 @@ var conductor: AudioStreamPlayer
 var timeline: Panel
 
 # This is a setter function that gets the length of the song in beat
-# This is calculated in "Paramaters.gd" under the load_song() function
+# This is calculated in "Paramaters.gd"
 # This also calls the timeline to re-draw the notes
 func length_set(value):
     song_length_beats = value
